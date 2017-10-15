460beca8260805d92337a86c0375ffefd7719b15
Matthew Castellana <castem4@rpi.edu>
2017-10-15 16:41:01 -0400

    Removed large iPhone build folder.

--- js/preload.js
+++ js/preload.js
@@ -21,9 +21,9 @@ preloadState.prototype.create = function() {
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
     game.scale.refresh();
-    game.state.start("titlemenu");
+    game.state.start("level1State");
 }
 
 preloadState.prototype.update = function() {
-    
+
 }
