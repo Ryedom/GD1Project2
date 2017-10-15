7f8169d9b510f0d1515b33aad8eb8cd47d1c2d35
Matthew Castellana <castem4@rpi.edu>
2017-10-15 02:17:19 -0400

    Enemy is now its own object. Created Level 1 State. Added/debugged enemy/player collision and health/damage. Modified player to use alternate OOP format due to compatibility issues with the built-in weapons class.

--- js/player.js
+++ js/player.js
@@ -1,9 +1,15 @@
 /** @constructor */
-let player = function()
+player = function(gamevar)
 {
+    // call super constructor on sprite
+
+
     this.playerYOffest = 200;
     this.playerVelocity = 200;
     this.playerIdleFrame = 4;
+    this.playerHealth = 1;
+    Phaser.Sprite.call(this, gamevar, game.world.width/2, game.world.height - this.playerYOffest, 'dude');
+    game.add.existing(this);
 
     this.bulletSpeed = 1500;
     this.fireRate = 0;
@@ -13,28 +19,15 @@ let player = function()
     this.bandMemberOffsetX = [-40, 40, -80, 80];
     this.bandMemberOffsetY = [40, 40, 80, 80];
 
-};
-
-player.prototype.preload = function()
-{
-
-};
-
-player.prototype.create = function()
-{
-
     //PLAYER---------------------------------------------------------------
-    //add this.player
-    this.player = game.add.sprite(game.world.width/2, game.world.height - this.playerYOffest, 'dude');
-
     //  We need to enable physics on the this.player
-    game.physics.arcade.enable(this.player);
+    game.physics.arcade.enable(this);
 
-    this.player.body.collideWorldBounds = true;
+    this.body.collideWorldBounds = true;
 
     //  Our two animations, walking left and right.
-    this.player.animations.add('left', [0, 1, 2, 3], 10, true);
-    this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+    this.animations.add('left', [0, 1, 2, 3], 10, true);
+    this.animations.add('right', [5, 6, 7, 8], 10, true);
     //END PLAYER----------------------------------------------------------
 
     //WEAPON--------------------------------------------------------------
@@ -60,7 +53,7 @@ player.prototype.create = function()
     //  X offset equal to half the player's width (so it fires from the center)
     //  no Y offset
     //  'false' argument tells the weapon not to track player rotation (will always shoot up)
-    this.playerWeapon.trackSprite(this.player, this.player.width/2, 0, false);
+    this.playerWeapon.trackSprite(this, this.width/2, 0, false);
     //END WEAPON----------------------------------------------------------------------
 
     //BAND MEMBERS------------------------------------------------------------------------
@@ -75,82 +68,61 @@ player.prototype.create = function()
     this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     game.input.keyboard.addKeyCapture([ Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT, Phaser.Keyboard.SPACEBAR ]);
 
-    // ENEMY CREATION LOGIC
+};
 
-    // create the groups of enemies
-    // skaters
-    this.skaters = game.add.group();
-    this.skaters.enableBody = true;
+player.prototype = Object.create(Phaser.Sprite.prototype);
+player.prototype.constructor = player;
 
-    // bullies
-    this.bullies = game.add.group();
-    this.bullies.enableBody = true;
+player.prototype.preload = function()
+{
 
-    //  create 3 skaters
-    for (let i = 0; i < 3; i++){
-      this.skaters.create(i * 70 + 10, 10, "dude");
-    }
+};
 
-    this.skaters.forEach(function(enem){
-      enem.tooClose = false; // used for preventing sprite from "hugging" player
-    }, this, true);
+player.prototype.create = function()
+{
 
-    // create a bully
-    for (let i = 0; i < 1; i++){
-      var bully = this.bullies.create(i * 70 + 400, 10, "dude");
-      playerControlTestState.prototype.enableWeaponForEnemy(bully);
-    }
 
-    // bully's projectiles - spitballs
-    this.spitballs = game.add.group();
-    this.spitballs.enableBody = true;
 };
 
 player.prototype.update = function()
 {
-    // COLLISION LOGIC
-    // enemy bullets hit player
-    this.bullies.forEach(playerControlTestState.prototype.enemyHitsPlayerCheck, this, true, this.player);
-
-    // player bullets hit enemy
-    this.bullies.forEach(playerControlTestState.prototype.playerHitsEnemyCheck, this, true, this.player);
 
     //then set the velocity depending on where the mouse is while button is down
     if(game.input.mousePointer.isDown)
     {
-        if(game.input.x < this.player.centerX)
+        if(game.input.x < this.centerX)
         {
-            this.player.body.velocity.x = -1 * this.playerVelocity;
+            this.body.velocity.x = -1 * this.playerVelocity;
 
-            this.player.animations.play('left');
+            this.animations.play('left');
         }
-        if(game.input.x > this.player.centerX)
+        if(game.input.x > this.centerX)
         {
-            this.player.body.velocity.x = this.playerVelocity;
+            this.body.velocity.x = this.playerVelocity;
 
-            this.player.animations.play('right');
+            this.animations.play('right');
         }
 
     }
     else
     {
         //  Reset the this.players velocity (movement)
-        this.player.body.velocity.x = 0;
+        this.body.velocity.x = 0;
 
         //  Stand still
-        this.player.animations.stop();
-        this.player.frame = this.playerIdleFrame;
+        this.animations.stop();
+        this.frame = this.playerIdleFrame;
     }
 
-    if (this.playerWeapon !== null) {
+    if (this.alive) {
       this.playerWeapon.fire();
     }
 
 
     //update allies' positions and make them fire
     for(i=0; i<this.bandMembers.children.length; i++){
-        this.bandMembers.children[i].x = this.player.x + this.bandMemberOffsetX[i];
-        this.bandMembers.children[i].y = this.player.y + this.bandMemberOffsetY[i];
+        this.bandMembers.children[i].x = this.x + this.bandMemberOffsetX[i];
+        this.bandMembers.children[i].y = this.y + this.bandMemberOffsetY[i];
 
         if (this.playerWeapon !== null) {
           this.playerWeapon.fireRate = 0;
@@ -169,18 +141,6 @@ player.prototype.update = function()
     if(this.spaceKey.downDuration(1)){
         this.killPlayer();
     }
-
-    // ENEMY UPDATES
-    // skaters move towards player
-    this.skaters.forEach(playerControlTestState.prototype.moveEnemiesTowardPlayer, this, true, this.player);
-
-    // bullies remain stationary but shoot towards player
-    this.bullies.forEach(playerControlTestState.prototype.shootAtPlayer, this, true, this.player);
-
-    // clear offscreen objects
-    this.skaters.forEach(playerControlTestState.prototype.destroyIfOffscreen, this, true);
-    this.bullies.forEach(playerControlTestState.prototype.destroyIfOffscreen, this, true);
-    this.spitballs.forEach(playerControlTestState.prototype.destroyIfOffscreen, this, true);
 };
 
 //debug text
@@ -216,78 +176,23 @@ player.prototype.removeBandMember = function(){
     }
 }
 
+player.prototype.getX = function(){
+  return this.body.x;
+}
+
+player.prototype.getY = function(){
+  return this.body.y;
+}
+
+player.prototype.damagePlayer = function(){
+  if (this.numBandMembers === 0){
+    this.killPlayer();
+  } else {
+    this.removeBandMember();
+  }
+
+}
+
 player.prototype.killPlayer = function(){
-    this.playerWeapon.destroy();
-    this.playerWeapon = null; // fire functions should check for this so they don't try to call the weapon
-    this.player.kill();
+    this.kill();
 }
-
-// ENEMY-RELATED FUNCTIONS
-
-playerControlTestState.prototype.getAngleToPlayer = function(enem, plyr) {
-  return Math.atan2(plyr.y - enem.y, plyr.x - enem.x);
-
-}
-
-// returns a distance (non-negative) from an enemy to a player
-playerControlTestState.prototype.getDistanceFromPlayer = function(enem, plyr) {
-  return Math.sqrt(Math.pow((enem.x - plyr.x), 2) + Math.pow((enem.y - plyr.y), 2));
-};
-
-playerControlTestState.prototype.moveEnemiesTowardPlayer = function(enem, plyr){
-  if (enem.tooClose === false){ // the enemy hasn't gotten too close
-    if (playerControlTestState.prototype.getDistanceFromPlayer(enem, plyr) > 20) { // within a certain distance, skater starts skating in a straight line
-      // find vector to player
-      var angleToPlayer = playerControlTestState.prototype.getAngleToPlayer(enem, plyr);
-      // set velocity to travel to her
-      game.physics.arcade.velocityFromRotation(angleToPlayer, 200, enem.body.velocity);
-    }
-    else { // set tooClose to true so enemy doesn't hug player
-        enem.tooClose = true;
-    }
-  }
-};
-
-playerControlTestState.prototype.shootAtPlayer = function(enem, plyr){
-    enem.weapon.fireAtSprite(plyr);
-}
-
-playerControlTestState.prototype.destroyIfOffscreen = function(obj){
-  if (!(obj.visible)){
-    obj.destroy();
-  }
-};
-
-playerControlTestState.prototype.enableWeaponForEnemy = function(enem){
-  var enemyWeapon = game.add.weapon(500, 'bullet');
-  //The 'rgblaser.png' is a Sprite Sheet with 80 frames in it (each 4x4 px in size)
-  //  The 3rd argument tells the Weapon Plugin to advance to the next frame each time
-  //  a bullet is fired, when it hits 80 it'll wrap to zero again.
-  //  You can also set this via this.weapon.bulletFrameCycle = true
-  enemyWeapon.setBulletFrames(0, 80, true);
-
-  //bullets disappear when they exit frame
-  enemyWeapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-
-  //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 50ms
-  enemyWeapon.fireRate = 1000;
-
-  enemyWeapon.trackSprite(enem, enem.width/2, 0, false);
-  enem.weapon = enemyWeapon;
-};
-
-playerControlTestState.prototype.enemyHitsPlayerCheck = function(enem, plyr){
-  game.physics.arcade.overlap(plyr, enem.weapon.bullets, playerControlTestState.prototype.enemyHitsPlayer, null, this);
-};
-
-playerControlTestState.prototype.playerHitsEnemyCheck = function(enem, plyr){
-  game.physics.arcade.overlap(enem, this.playerWeapon.bullets, playerControlTestState.prototype.playerHitsEnemy, null, this);
-};
-
-playerControlTestState.prototype.enemyHitsPlayer = function(plyr, bull) {
-   this.killPlayer();
-};
-
-playerControlTestState.prototype.playerHitsEnemy = function(enem, bull) {
-  // check which group the enemy is in, and remove him from that group
-};
--- js/preload.js
+++ js/preload.js
@@ -16,7 +16,7 @@ preloadState.prototype.preload = function()
 
 preloadState.prototype.create = function()
 {
-    game.state.start("PlayerObjectTestState");
+    game.state.start("level1State");
 };
 
 preloadState.prototype.update = function()
