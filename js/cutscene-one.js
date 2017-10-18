d3e2c289bc14df5ff90a08f03fe81d59ac43333f
Alexander Christner <christner.alex@gmail.com>
2017-10-17 22:49:04 -0400

    changed cutscenes 1 and 4 to match existing folder structure

--- js/cutscene-one.js
+++ js/cutscene-one.js
@@ -4,12 +4,12 @@ let cutOneState = function() {
 
 cutOneState.prototype.preload = function() {
     this.framePaths = [
-        "assets/Cutscene1/cutscene1_1.png",
-        "assets/Cutscene1/cutscene1_2.png",
-        "assets/Cutscene1/cutscene1_3.png",
-        "assets/Cutscene1/cutscene1_4.png",
-        "assets/Cutscene1/cutscene1_5.png",
-        "assets/Cutscene1/cutscene1_6.png"
+        "assets/Cutscenes/Cutscene1/cutscene1_1.png",
+        "assets/Cutscenes/Cutscene1/cutscene1_2.png",
+        "assets/Cutscenes/Cutscene1/cutscene1_3.png",
+        "assets/Cutscenes/Cutscene1/cutscene1_4.png",
+        "assets/Cutscenes/Cutscene1/cutscene1_5.png",
+        "assets/Cutscenes/Cutscene1/cutscene1_6.png"
     ]
     
     this.frameSprites = [];
